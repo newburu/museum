b50ac4af172e36ba45556535019d3bf4d9835c03
Newburu <newburu@gmail.com>
2022-04-03 15:27:24 +0900

    フォローボタンを実装

--- app/models/museum.rb
+++ app/models/museum.rb
@@ -1,3 +1,10 @@
 class Museum < ApplicationRecord
   belongs_to :user
+  has_many :follows, dependent: :destroy
+  has_many :following_users, through: :follows, source: :user
+
+  def followed_by?(user)
+    following_users.include?(user)
+  end
+
 end
